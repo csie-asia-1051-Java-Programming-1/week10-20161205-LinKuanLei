95c355452bca4d1ef6967b7f4edda4569fffa63f
LinKuanLei <103051089@live.asia.edu.tw>
2016-12-10 10:00:25 -0600

    ex03_submit

--- src/ex/ex03_103051089.java
+++ src/ex/ex03_103051089.java
@@ -1,9 +1,3 @@
-﻿package ex;
-/*
- * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
- * Date: 2016/12/05
- * Author: 103051089 林冠磊
- */
 import java.util.Scanner;
 import java.util.ArrayList;
 public class ex03_103051089 {
@@ -12,18 +6,15 @@ public class ex03_103051089 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Person> perList = new ArrayList<Person>();
-		String name ;
-		String sex;
-		float hight;
-		float weight;
+		
 		char chk;
 		boolean sw =true;
 		while(sw){
 			System.out.println("(姓名,性別,身高,體重)");
-			name = sc.next();
-			sex = sc.next();
-			hight = sc.nextFloat();
-			 weight = sc.nextFloat();
+			String name = sc.next();
+			String sex = sc.next();
+			float hight = sc.nextFloat();
+			float weight = sc.nextFloat();
 			while(true){
 				if(hight>0){
 					break;
@@ -40,26 +31,64 @@ public class ex03_103051089 {
 					weight = sc.nextFloat();
 				}
 			}
-			perList.add(new Person(name,sex,hight,weight));
+			Person tmp = new Person(name,sex,hight,weight);
+			
+			if(perList.size()==0)
+			{
+				perList.add(tmp);
+			}
+			else if(perList.size()==1)
+			{
+				if(perList.get(0).getBMI()<tmp.getBMI())
+				{
+					perList.add(0,tmp);
+				}
+				else
+				{
+					perList.add(tmp);
+				}
+			}
+			else
+			{
+				boolean flog = true;
+				for(int i=0;i<perList.size()-1 && flog;i++){
+					if(i==0 && tmp.getBMI() > perList.get(i).getBMI())
+					{
+						perList.add(0,tmp);
+						flog = false;
+					}
+					else
+					{
+						if(tmp.getBMI() < perList.get(i).getBMI() && tmp.getBMI() >= perList.get(i+1).getBMI())
+						{
+							perList.add(i+1,tmp);
+							flog = false;
+						}else if(perList.get(perList.size()-1).getBMI() > tmp.getBMI()){
+							perList.add(tmp);
+							flog = false;
+						}
+						
+					}
+				}
+			}
 			showPerson(perList);
-
+			
 			System.out.println("more?");
 			chk =sc.next().charAt(0) ;
 			if(chk=='N' || chk== 'n' ){
 				sw=false;
+				System.out.println("bye");
 			}
 		}
 	}
 
 	private static void showPerson(ArrayList list1){
-			for(int i =0;i<list1.size();i++){
-					Person tmp = (Person)	list1.get(i);
-					tmp.ShowInfo();
-
-//				System.out.println(list.get(i).ShowInfo());
-			}
+		for(int i =0;i<list1.size();i++){
+			Person tmp = (Person) list1.get(i);
+			tmp.ShowInfo();
 		}
-	
+	}
+
 }
 
 
@@ -72,11 +101,11 @@ class Person{
 		hight = hight1;
 		weight = weight1;
 	}
-	
+
 	public double getBMI(){
 		return weight/Math.pow(hight/100, 2);
 	}
-	
+
 	public void ShowInfo(){
 		System.out.println(name+"\t"+sex+"\t"+hight+"\t"+weight+"\t\t"+getBMI());
 	}
